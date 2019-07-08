b08022e43bd418faa5718a75fd4ffd90bde8287e
sir_Andros <andros.osipkov@gmail.com>
2019-07-08 14:17:43 +0200

    Move sources to src folder

--- Pack-Plugin.ps1
+++ Pack-Plugin.ps1
@@ -1,6 +1,7 @@
 param (
     [string] $ProjectDir = $null,
-    [string] $TempDirName = 'KeePassWinHelloPlugin'
+    [string] $TempDirName = 'KeePassWinHelloPlugin',
+    [string] $OutputDir = $null
 )
 $keePassExe = 'C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe'
 
@@ -10,18 +11,39 @@ if (!$PSScriptRoot) {
 if (!$ProjectDir) {
     $ProjectDir = $PSScriptRoot
 }
+if (!$OutputDir) {
+    $OutputDir = "$ProjectDir\out"
+}
 
-$dest = "$ProjectDir\$TempDirName"
+$sources = "$ProjectDir\src"
+
+
+if (!(Test-Path $OutputDir)) {
+    mkdir $OutputDir > $null
+}
+
+$dest = "$OutputDir\$TempDirName"
 if (Test-Path $dest) {
     rm $dest -Force -Recurse
 }
 mkdir $dest > $null
 
+if (!(Test-Path $OutputDir)) {
+    mkdir $OutputDir > $null
+}
+
 $ExcludedItems = '.*', '*.sln', 'bin', 'obj', '*.user', '*.ps1', '*.plgx', '*.md', $TempDirName, 'Screenshots'
-dir $ProjectDir | 
+dir $sources | 
     ?{ $i=$_; $res=$true; $ExcludedItems | %{ $i -notlike $_ } | %{ $res = $_ -and $res }; $res } |
     %{ copy $_.FullName $dest -Recurse }
 
-Start-Process $keePassExe -arg '--plgx-create',"`"$dest`"" -Wait
+try {
+    Push-Location
+    cd $OutputDir
+    Start-Process $keePassExe -arg '--plgx-create',"`"$dest`"" -Wait
+} finally {
+    Pop-Location
+}
+
 
 rm $dest -Force -Recurse
