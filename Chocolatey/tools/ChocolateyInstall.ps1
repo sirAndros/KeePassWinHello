e75afad081cb52e570de86bad296cd641d35f809
sir_Andros <andros.osipkov@gmail.com>
2019-07-18 15:00:31 +0200

    Chages for comply Chocolatey modaration

--- Chocolatey/tools/ChocolateyInstall.ps1
+++ Chocolatey/tools/ChocolateyInstall.ps1
@@ -8,69 +8,53 @@ if ($psver -ge 3) {
 
 $packageName = 'keepass-plugin-winhello'
 $keePassDisplayName = 'KeePass Password Safe'
-$version = '2.2'
-$checksum = 'A38E54462C9DF1B370DB269C17D4225B2F5A4FB9F6595EAEE60CFF1ABEE807D4'
-$checksumType = 'sha256'
-$pluginFileName = 'KeePassWinHelloPlugin.plgx'
-$url = "https://github.com/sirAndros/KeePassWinHello/releases/download/v$version/$pluginFileName"
 
-try {
-    Write-Verbose "Searching registry for installed KeePass..."
-    $regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                        'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                        'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                -ErrorAction:SilentlyContinue `
-            | Where-Object { $_.DisplayName -like "$keePassDisplayName*" } `
-            | ForEach-Object { $_.InstallLocation }
+
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+            -ErrorAction:SilentlyContinue `
+        | Where-Object { $_.DisplayName -like "$keePassDisplayName*" } `
+        | ForEach-Object { $_.InstallLocation }
     
-    $installPath = $regPath #todo process multiple installations
-    if (!$installPath) {
-        Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
-        $binRoot = Get-BinRoot
-        $portPath = Join-Path $binRoot "keepass"
-        $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+$installPath = $regPath #todo process multiple installations
+if (!$installPath) {
+    Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+    $binRoot = Get-BinRoot
+    $portPath = Join-Path $binRoot "keepass"
+    $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (!$installPath) {
+    Write-Verbose "Searching $env:Path for unregistered install..."
+    $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+    if (! $installFullName) {
+        $installPath = [io.path]::GetDirectoryName($installFullName)
     }
-    if (!$installPath) {
-        Write-Verbose "Searching $env:Path for unregistered install..."
-        $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
-        if (! $installFullName) {
-            $installPath = [io.path]::GetDirectoryName($installFullName)
-        }
-    }
-    if (!$installPath) {
-        Write-Warning "$keePassDisplayName not found."
-        throw
-    }
-    Write-Verbose "`t...found."
+}
+if (!$installPath) {
+    Write-Warning "$keePassDisplayName not found."
+    throw
+}
+Write-Verbose "`t...found."
 
-    Write-Verbose "Searching for plugin directory..."
-    $pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
-    if ($pluginPath.Count -eq 0) {
-        $pluginPath = Join-Path $installPath "Plugins"
-        [System.IO.Directory]::CreateDirectory($pluginPath)
-    }
-    Write-Verbose "`t...found."
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+    $pluginPath = Join-Path $installPath "Plugins"
+    [System.IO.Directory]::CreateDirectory($pluginPath)
+}
+Write-Verbose "`t...found."
 
-    $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    $source = "$toolsDir\..\plugin"
-    if (!(Test-Path $source)) {
-        Write-Verbose "Download plugin files"
-        Get-ChocolateyWebFile -PackageName "$packageName" `
-                              -Url "$url" `
-                              -FileFullPath  "$source\$pluginFileName" `
-                              -Checksum "$checksum" `
-                              -ChecksumType "$checksumType"
-    }
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$source = "$toolsDir\..\plugin"
     
-    Write-Verbose "Copy plugin files into Plugins dir"
-    Copy-Item -Path "$source\*" -Destination $pluginPath -Recurse -Force
+Write-Verbose "Copy plugin files into Plugins dir"
+Copy-Item -Path "$source\*" -Destination $pluginPath -Recurse -Force
 
-    if ( Get-Process -Name "KeePass" -ErrorAction SilentlyContinue ) {
-        Write-Warning "$keePassDisplayName is currently running. Plugin will be available at next restart of KeePass process." 
-    } else {
-        Write-Host "$packageName will be loaded the next time KeePass is started."
-        Write-Host "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Tools>Options"
-    }
-} catch {
-    throw $_.Exception
+if ( Get-Process -Name "KeePass" -ErrorAction SilentlyContinue ) {
+    Write-Warning "$keePassDisplayName is currently running. Plugin will be available at next restart of KeePass process." 
+} else {
+    Write-Host "$packageName will be loaded the next time KeePass is started."
+    Write-Host "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Tools>Options"
 }
--- Chocolatey/tools/ChocolateyUninstall.ps1
+++ Chocolatey/tools/ChocolateyUninstall.ps1
@@ -10,54 +10,50 @@ else {
 $keePassDisplayName = 'KeePass Password Safe'
 $files = @("KeePassWinHelloPlugin.plgx")
 
-try {
-    Write-Verbose "Checking KeePass is not running..."
-    if (Get-Process -Name "KeePass" -ErrorAction SilentlyContinue) {
-        Write-Warning "$($keePassDisplayName) is running. Please save any opened databases and close $($keePassDisplayName) before attempting to uninstall KeePass plugins."
-        throw
-    }
+Write-Verbose "Checking KeePass is not running..."
+if (Get-Process -Name "KeePass" -ErrorAction SilentlyContinue) {
+    Write-Warning "$($keePassDisplayName) is running. Please save any opened databases and close $($keePassDisplayName) before attempting to uninstall KeePass plugins."
+    throw
+}
 
-    Write-Verbose "Searching registry for installed KeePass..."
-    $regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                        'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                        'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                    -ErrorAction:SilentlyContinue `
-                | Where-Object { $_.DisplayName -like "$keePassDisplayName*" } `
-                | ForEach-Object { $_.InstallLocation }
-    $installPath = $regPath
-    if (!$installPath) {
-        Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
-        $binRoot = Get-BinRoot
-        $portPath = Join-Path $binRoot "keepass"
-        $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                -ErrorAction:SilentlyContinue `
+            | Where-Object { $_.DisplayName -like "$keePassDisplayName*" } `
+            | ForEach-Object { $_.InstallLocation }
+$installPath = $regPath
+if (!$installPath) {
+    Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+    $binRoot = Get-BinRoot
+    $portPath = Join-Path $binRoot "keepass"
+    $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (!$installPath) {
+    Write-Verbose "Searching $env:Path for unregistered install..."
+    $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+    if (! $installFullName) {
+        $installPath = [io.path]::GetDirectoryName($installFullName)
     }
-    if (!$installPath) {
-        Write-Verbose "Searching $env:Path for unregistered install..."
-        $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
-        if (! $installFullName) {
-            $installPath = [io.path]::GetDirectoryName($installFullName)
-        }
-    }
-    if (!$installPath) {
-        Write-Warning "$($keePassDisplayName) not found."
-        throw
-    }
-    Write-Verbose "`t...found."
+}
+if (!$installPath) {
+    Write-Warning "$($keePassDisplayName) not found."
+    throw
+}
+Write-Verbose "`t...found."
 
-    Write-Verbose "Searching for plugin directory..."
-    $pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
-    if ($pluginPath.Count -eq 0) {
-        throw "Plugin directory not found."
-    }
-    Write-Verbose "`t...found."
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+    throw "Plugin directory not found."
+}
+Write-Verbose "`t...found."
 
-    Write-Verbose "Removing plugin files..."
-    foreach ($file in $files) {
-        $installFile = Join-Path $pluginPath $file
-        Remove-Item -Path $installFile `
-                    -Force `
-                    -ErrorAction SilentlyContinue
-    }
-} catch {
-    throw $_.Exception
+Write-Verbose "Removing plugin files..."
+foreach ($file in $files) {
+    $installFile = Join-Path $pluginPath $file
+    Remove-Item -Path $installFile `
+                -Force `
+                -ErrorAction SilentlyContinue
 }
