12d7ea462c197eac562aa8ae387d4c25aa99269c
Sasha Sitnikov <sasha@sitnikov.in>
2019-08-24 15:08:22 +0200

    Added exception types

---
+++ Exceptions/AuthProviderException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KeePassWinHello
+{
+    [Serializable]
+    public class AuthProviderException : KeePassWinHelloException
+    {
+        public AuthProviderException() { }
+        public AuthProviderException(string message) : base(message) { }
+        public AuthProviderException(string message, Exception inner) : base(message, inner) { }
+        protected AuthProviderException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
