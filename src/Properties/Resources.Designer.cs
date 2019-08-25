01d9f422dd7aec06936124aa982ac59212d3531e
sir_Andros <andros.osipkov@gmail.com>
2019-08-26 00:08:00 +0200

    Adjust size of plugin icon for different monitors

--- src/Properties/Resources.Designer.cs
+++ src/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace KeePassWinHello.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap KPWH_16x16 {
+            get {
+                object obj = ResourceManager.GetObject("KPWH_16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap KPWH_32x32 {
             get {
                 object obj = ResourceManager.GetObject("KPWH_32x32", resourceCulture);
