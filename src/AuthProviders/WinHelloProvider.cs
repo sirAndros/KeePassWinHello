2f8b3ec1af5d131f370b239e109887fac1d10081
Sasha Sitnikov <sasha@sitnikov.in>
2019-09-01 14:37:35 +0200

    Removed unused using

--- src/AuthProviders/WinHelloProvider.cs
+++ src/AuthProviders/WinHelloProvider.cs
@@ -2,7 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Principal;
-using KeePassWinHello.Utilities;
 using Microsoft.Win32.SafeHandles;
 
 namespace KeePassWinHello
